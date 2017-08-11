125d658b979bb0ca7e6a18a32197f7cd7a763b8f
Kanbaru <lihaochen910@hotmail.com>
2017-08-12 00:22:20 +0800

    添加地图类查询/操作方法

--- Scripts/MapMapper.cs
+++ Scripts/MapMapper.cs
@@ -3,29 +3,80 @@ using System.Collections;
 
 public sealed class MapMapper : MonoBehaviour
 {
-    private const string BoxTag = "Box";
-    private GameObject[,] Map;
-    void Awake()
+    private const string            BoxTag = "Box";
+    private readonly Vector2        nilPosition = new Vector2(-1, -1);
+
+    public static MapMapper         ins = null;
+
+    private Box[,]           Map = null;
+
+    public Box this[int x,int y] {
+        get { return Map[x, y]; }
+        //private set {  }
+    }
+    public Box this[float x, float y]
     {
-        Map = InitMap();
+        get { return Map[(int)x, (int)y]; }
     }
 
-    GameObject[,] InitMap()
+    #region MonoBehaviour
+    void Awake()
+    {
+        ins = this;
+        Map = InitMap();
+    }
+    #endregion
+
+    /* 初始化地图数组 */
+    Box[,] InitMap()
     {
         var boxes = GameObject.FindGameObjectsWithTag(BoxTag);
 
-        var map = new GameObject[99, 99];
+        var map = new Box[30, 30];
 
         foreach (var box in boxes)
         {
             var x = box.transform.position.x;
             var y = box.transform.position.y;
-            map[(int)x, (int)y] = box;
+            map[(int)x, (int)y] = box.GetComponent<Box>();
         }
 
         return map;
     }
-    /* Scene视图 */
+    /* 索引是否在地图范围中 */
+    public bool InRange(Vector2 position)
+    {
+        var x = position.x;
+        var y = position.y;
+
+        if (x > Map.GetLength(0))
+            return false;
+        if (y > Map.GetLength(1))
+            return false;
+
+        return true;
+    }
+    /* 获取元素在地图中的索引 */
+    public Vector2 GetBoxIndex(Box box)
+    {
+        for(int x = 0;x < Map.GetLength(0); x++)
+            for (int y = 0; y < Map.GetLength(1); y++)
+                if (Map[x, y] == box)
+                    return new Vector2(x, y);
+        return nilPosition;
+    }
+    /* 移动地图中的元素 */
+    public void MoveTo(Box box,Vector2 destination)
+    {
+        if (InRange(destination))
+        {
+            var index = GetBoxIndex(box);
+            Map[(int)destination.x, (int)destination.y] = box;
+            Map[(int)index.x, (int)index.y] = null;
+        }
+    }
+
+    #region DrawOnSceneView
     void OnDrawGizmos()
     {
         DrawMapRange();
@@ -46,7 +97,6 @@ public sealed class MapMapper : MonoBehaviour
             }
         }
     }
-
     void DrawMapRange()
     {
         if (Map == null)
@@ -60,4 +110,5 @@ public sealed class MapMapper : MonoBehaviour
             }
         }
     }
+    #endregion
 }
