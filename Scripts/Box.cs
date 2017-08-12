4f003a5a24720a4fca36d33f337417c2716ff3f4
lihaochen910 <liahochen910@hotmail.com>
2017-08-12 16:58:52 +0800

    添加 遍历测试代码

--- Scripts/Box.cs
+++ Scripts/Box.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using HighlightingSystem;
+using System.Collections;
 
 public class Box : MonoBehaviour
 {
@@ -7,7 +9,6 @@ public class Box : MonoBehaviour
      *  (1)抛出消息/收到消息后处理什么
      *  (2)Box状态切换/检查
      *  (3)如何同步下落
-     *  (4)怎么知道所有受影响的Box已经都收到消息
      */
     #region Private variables
     private MapMapper _map;
@@ -31,5 +32,50 @@ public class Box : MonoBehaviour
     }
     #endregion
 
-    
+    #region TempCode
+    public void OnMoveEnd()
+    {
+        Send();
+        print("end");
+    }
+    public void Send()
+    {
+        var map = _map.GetMap();
+        var origin = _map.GetBoxIndex(this);
+
+        var boxes = new Box[3];
+
+        var start = -1;
+        for (int i = 0; i < boxes.Length; i++)
+        {
+            boxes[i] = _map[origin.x + start, origin.y + 1];
+            start++;
+        }
+
+        foreach (var box in boxes)
+        {
+            if (box)
+                box.Recive();
+        }
+        
+
+    }
+    public void Recive()
+    {
+        Debug.LogWarning(gameObject.name + " OnRecive");
+        Send();
+        StartCoroutine(light());
+    }
+    IEnumerator light()
+    {
+        var hler = GetComponent<Highlighter>();
+        if (hler == null)
+        {
+            hler = gameObject.AddComponent<Highlighter>();
+        }
+        hler.ConstantOn(Color.white);
+        yield return new WaitForSeconds(0.7f);
+        hler.ConstantOff();
+    }
+    #endregion
 }
